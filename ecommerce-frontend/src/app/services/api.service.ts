a2e94c037a50775c70a60d66af5145160a37c853
Daniel <danieleduardo.rm9@gmail.com>
2025-01-07 18:46:35 -0500

    Ajustando conexion con el backend y librerias

--- ecommerce-frontend/src/app/services/api.service.ts
+++ ecommerce-frontend/src/app/services/api.service.ts
@@ -3,14 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
 
-  private baseUrl= 'http://localhost:5000/api';
+  
 
 
   constructor(private http: HttpClient) { }
+
+  private baseUrl= 'http://localhost:5000/api';
 
   getProducts(): Observable <any> {
     return this.http.get(`${this.baseUrl}/products`);
